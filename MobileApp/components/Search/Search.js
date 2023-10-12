204b0f5641ef12e6e8260f691293a20a2ea617fb
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-12 09:47:26 -0300

    search en proceso

--- MobileApp/components/Search/Search.js
+++ MobileApp/components/Search/Search.js
@@ -1,33 +1,44 @@
 import { SearchBar } from 'react-native-elements';
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import axios from 'axios'; 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-
     const baseURL = "http://localhost:3000/getContactList";
-    const [ListaContactos, setListaContactos] = useState([{}]);
+    const [contactList, setContactList] = useState([]);
+    const [search, setSearch] = useState('');
+    const [suggestedContacts, setSuggestedContacts] = useState([]);
 
     useEffect(() => {
         axios.get(baseURL).then((response) => {
-            setListaContactos(response.data);
-            console.log(listaEventos);
-            sendlistaEventos(listaEventos);
+            setContactList(response.data);
         });
     }, []);
 
-    updateSearch = (search) => {
-        this.setState({ search });
+    const updateSearch = (text) => {
+        setSearch(text);
+        // Filter the contact list based on the first three characters of the search input
+        if (text.length >= 3) {
+            const filteredContacts = contactList.filter(contact =>
+                contact.nombre.toLowerCase().includes(text.toLowerCase())
+            );
+            setSuggestedContacts(filteredContacts);
+        } else {
+            setSuggestedContacts([]);
+        }
     };
 
-    const { search } = this.state;
-
     return (
-        <SearchBar
-            placeholder="Ingrese Nombre"
-            onChangeText={this.updateSearch}
-            value={search}
-        />
+        <>
+            <SearchBar
+                placeholder="Ingrese Nombre"
+                onChangeText={updateSearch}
+                value={search}
+            />
+            {/* Display suggested contacts */}
+            {suggestedContacts.map(contact => (
+                <Text key={contact.id}>{contact.name}</Text>
+            ))}
+        </>
     );
 }
 
